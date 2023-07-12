4eb3808aecc80439acefc96e8dee3ed225875326
Chris O'Brian <mobiletread@gmail.com>
2023-07-11 19:48:17 -0700

    updates to the function for refs

--- src/lib.rs
+++ src/lib.rs
@@ -4,6 +4,8 @@
 // 3. It's the end
 // 4. Has been seen already
 
+use std::str::Chars;
+
 // Represents North, East, South and West
 const DIR: [[i32; 2]; 4] = [[0, 1], [1, 0], [0, -1], [-1, 0]];
 
@@ -20,12 +22,12 @@ impl Point {
 }
 
 pub fn walk(
-    maze: Vec<Vec<String>>,
-    wall: String,
+    maze: Vec<Vec<char>>,
+    wall: char,
     curr: Point,
     end: Point,
-    seen: Vec<Vec<bool>>,
-    mut path: Vec<Point>,
+    seen: &mut Vec<Vec<bool>>,
+    path: &mut Vec<Point>,
 ) -> bool {
     if (curr.x as i32) < 0 || curr.x as usize >= maze[0].len() {
         return false;
@@ -46,6 +48,7 @@ pub fn walk(
 
     //pre
     path.push(curr.clone());
+    seen[curr.y as usize][curr.x as usize] = true;
     //recurse
     for i in DIR.iter() {
         let (x, y) = (i[0], i[1]);
@@ -54,8 +57,8 @@ pub fn walk(
             wall.clone(),
             Point::new(curr.x + x, curr.y + y),
             end.clone(),
-            seen.clone(),
-            path.clone(),
+            seen,
+            path,
         ) {
             return true;
         };
@@ -65,9 +68,26 @@ pub fn walk(
 
     false
 }
+
+pub fn solve(maze: Vec<Vec<char>>, wall: char, start: Point, end: Point) -> Vec<Point> {
+    let mut seen = vec![vec![false; maze[0].len()]; maze.len()];
+    let mut path = Vec::<Point>::new();
+
+    walk(maze, wall, start, end, &mut seen, &mut path);
+    path
+}
+
 #[cfg(test)]
 mod tests {
 
     #[test]
-    fn it_works() {}
+    fn amazing_maze() {
+        let mut maze: Vec<Vec<char>> = Vec::new();
+        maze.push("xxxxxxxxxx x".chars().collect());
+        maze.push("x        x x".chars().collect());
+        maze.push("x        x x".chars().collect());
+        maze.push("x xxxxxxxx x".chars().collect());
+        maze.push("x          x".chars().collect());
+        maze.push("x xxxxxxxxxx".chars().collect());
+    }
 }
